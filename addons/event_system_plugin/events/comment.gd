333b670bb396a9121708a66d358506729f840abd
AnidemDex <dav.medrin@gmail.com>
2021-11-01 21:49:33 -0500

    Add Comment event

---
+++ addons/event_system_plugin/events/comment.gd
@@ -0,0 +1,21 @@
+tool
+extends Event
+
+export(String, MULTILINE) var text:String = "" setget set_text
+
+func _init() -> void:
+	event_name = "Comment"
+	event_preview_string = "# {text}"
+	event_icon = load("res://addons/event_system_plugin/assets/icons/event_icons/comment_event.png") as Texture
+	event_hint = "Makes comment in the timeline.\nThis doesn't affects the timeline behaviour."
+	event_color = Color("#3C3D5E")
+
+
+func _execute(_event_node=null) -> void:
+	finish()
+
+
+func set_text(value:String) -> void:
+	text = value
+	property_list_changed_notify()
+	emit_changed()
