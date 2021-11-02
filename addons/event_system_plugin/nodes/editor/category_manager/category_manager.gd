7e1b0a930173e3bc74d933ca0f69c3ff5ec9162d
AnidemDex <dav.medrin@gmail.com>
2021-11-01 21:50:49 -0500

    Add Category and CategoryManager

---
+++ addons/event_system_plugin/nodes/editor/category_manager/category_manager.gd
@@ -0,0 +1,36 @@
+extends PanelContainer
+
+signal event_pressed(event)
+
+export(PackedScene) var category:PackedScene
+export(NodePath) var DummyContainer:NodePath
+
+var categories:Dictionary = {}
+
+onready var categories_container:Control = get_node(DummyContainer) as Control
+
+
+func add_event(event_script:Script) -> void:
+	var event:Resource = event_script.new()
+	var event_category:String = str(event.get("event_category"))
+	
+	if not(event_category in categories):
+		add_category(event_category)
+	
+	var category_node = categories[event_category]
+	category_node.call("add_event", event)
+
+
+func add_category(category_name:String) -> void:
+	var category_node:Node = category.instance()
+	category_node.set("category", category_name)
+	category_node.connect("ready", category_node, "_fake_ready")
+	category_node.connect("event_pressed", self, "_on_EventButton_pressed")
+	
+	categories[category_name] = category_node
+	
+	categories_container.add_child(category_node)
+
+
+func _on_EventButton_pressed(event_script:Script) -> void:
+	emit_signal("event_pressed", event_script.new())
