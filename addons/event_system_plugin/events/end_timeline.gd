24ab0db97f6fe81672ffdee5f196ae2a2b07396b
Dex <dav.medrin@gmail.com>
2022-01-11 12:24:27 -0500

    Rename return.gd to end_timeline.gd

--- addons/event_system_plugin/events/end_timeline.gd
+++ addons/event_system_plugin/events/end_timeline.gd
@@ -2,7 +2,7 @@ extends Event
 
 func _init() -> void:
 	event_color = Color("EB5E55")
-	event_name = "Return"
+	event_name = "End Timeline"
 	event_hint = "Terminates the execution of the timeline at this point"
 	event_icon = load("res://addons/event_system_plugin/assets/icons/event_icons/change_timeline.png") as Texture
 
