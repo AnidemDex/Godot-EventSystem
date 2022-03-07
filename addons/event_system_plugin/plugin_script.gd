cf69f38d20cde8b76dc31b71b1b990269d43b04a
AnidemDex <dav.medrin@gmail.com>
2022-03-07 16:30:16 -0500

    Re-add welcome popup.
    Now is named "main_panel"

--- addons/event_system_plugin/plugin_script.gd
+++ addons/event_system_plugin/plugin_script.gd
@@ -11,16 +11,16 @@ var timeline_dock_button:ToolButton
 
 func _enter_tree():
 	show_plugin_version_button()
-	
-	timeline_editor = TimelineEditor.new()
-	register_plugin_node(timeline_editor)
 	timeline_dock_button = add_control_to_bottom_panel(timeline_editor, "TimelineEditor")
 	timeline_dock_button.hide()
-	
-	connect("scene_changed", self, "_scene_change")
 
 
 func _enable_plugin():
+	var main_panel:PanelContainer = load("res://addons/event_system_plugin/nodes/editor/welcome/main_panel.tscn").instance()
+	main_panel.set("repository", get_plugin_repository())
+	main_panel.set("docs", get_plugin_docs_url())
+	main_panel.set("version", get_plugin_version())
+	get_plugin_welcome_node().get_tab_by_idx(0).add_child(main_panel)
 	show_welcome_node()
 
 
@@ -35,18 +35,38 @@ func _handles(object: Object) -> bool:
 
 
 func _edit(object: Object) -> void:
-	timeline_editor.set_undo_redo(get_undo_redo())
-	var timeline:Resource = object.get("timeline")
-	if !timeline:
-		return
-	timeline_editor.edit_timeline(timeline)
+	
+	if object is EventManagerClass:
+		timeline_editor.set_undo_redo(get_undo_redo())
+		var timeline:Resource = object.get("timeline")
+		if !timeline:
+			return
+		timeline_editor.edit_timeline(timeline)
 
 
 func _make_visible(visible: bool) -> void:
 	if visible:
 		timeline_dock_button.show()
 		make_bottom_panel_item_visible(timeline_editor)
+		timeline_editor.set_process_input(true)
 	else:
 		if timeline_dock_button.is_visible_in_tree():
 			hide_bottom_panel()
 		timeline_dock_button.hide()
+		timeline_editor.set_process_input(false)
+
+
+func _scene_change(scene_root:Node) -> void:
+	pass
+
+
+func _on_TimelineEditor_event_selected(event:EventClass) -> void:
+	get_editor_interface().edit_resource(event)
+
+
+func _init() -> void:
+	timeline_editor = TimelineEditor.new()
+	timeline_editor.connect("event_selected", self, "_on_TimelineEditor_event_selected")
+	register_plugin_node(timeline_editor)
+	
+	connect("scene_changed", self, "_scene_change")
