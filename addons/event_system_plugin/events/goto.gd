414fabc60bb5407bbd7bd00809ae5222f1d99062
AnidemDex <dav.medrin@gmail.com>
2022-04-17 21:16:35 -0500

    Expose next_event in goto event

--- addons/event_system_plugin/events/goto.gd
+++ addons/event_system_plugin/events/goto.gd
@@ -6,7 +6,19 @@ func _init() -> void:
 	event_name = "Go to Event"
 	event_color = Color("#FBB13C")
 	event_icon = load("res://addons/event_system_plugin/assets/icons/event_icons/jump_to_event.png") as Texture
-	event_preview_string = "Go to event [???]"
+	event_preview_string = "Go to event [{next_event}]"
 	continue_at_end = true
 	event_category = "Logic"
 	event_hint = "Helper event to define the next event after this event"
+
+func _get(property):
+	if property == "event_node_path_ignore":
+		return true
+	if property == "continue_at_end_ignore":
+		return true
+
+
+func _get_property_list():
+	var p := []
+	p.append({"name":"next_event", "type":TYPE_OBJECT, "hint":PROPERTY_HINT_RESOURCE_TYPE})
+	return p
